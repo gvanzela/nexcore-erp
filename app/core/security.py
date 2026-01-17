ab9a5e5b1105639a4311987262f611cd9a35639d
gvanzela <gabriel.vanzela@hotmail.com>
2026-01-17 07:03:36 -0300

    refactor(auth): switch to HTTP Bearer auth for JWT

--- app/core/security.py
+++ app/core/security.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 from jose import jwt, JWTError
 import secrets
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 
 from app.core.config import settings
@@ -80,11 +80,11 @@ def verify_access_token(token: str) -> str:
 
 # OAuth2 scheme definition
 # Reads the JWT from: Authorization: Bearer <token>
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+security = HTTPBearer()
 
 
 def get_current_user(
-    token: str = Depends(oauth2_scheme),
+    credentials: HTTPAuthorizationCredentials = Depends(security),
     db: Session = Depends(get_db),
 ) -> User:
     """
@@ -98,6 +98,9 @@ def get_current_user(
     Any endpoint using this dependency
     automatically requires authentication.
     """
+
+    # Extract rae JWT from Authorization header
+    token = credentials.credentials
 
     # Decode and validate JWT
     try:
